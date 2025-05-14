1eaf469eae700a7165dc4369f636e01d40f9469f
José Manuel Luque <jluqgon214@g.educaand.es>
2025-05-14 16:29:47 +0200

    Actualizar Pantalla Pull

--- app/src/main/java/com/jluqgon214/hogarmate/screens/TasksScreen.kt
+++ app/src/main/java/com/jluqgon214/hogarmate/screens/TasksScreen.kt
@@ -1,27 +1,45 @@
 package com.jluqgon214.hogarmate.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialShapes
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import com.jluqgon214.hogarmate.components.AddTaskDialog
 import com.jluqgon214.hogarmate.components.CardTarea
 import com.jluqgon214.hogarmate.viewModel.TasksViewModel
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TasksScreen(tasksViewModel: TasksViewModel, paddingValues: PaddingValues) {
-    val tareas by tasksViewModel.tareas.collectAsState()
+    // val tareas by tasksViewModel.tareas.collectAsState()
     val tareasCargando by tasksViewModel.tareasCargando.collectAsState()
 
     val tareasPendientes by tasksViewModel.tareasPendientes.collectAsState()
@@ -51,55 +69,108 @@ fun TasksScreen(tasksViewModel: TasksViewModel, paddingValues: PaddingValues) {
     }
 
 
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(paddingValues)
-    ) {
-        item {
-
-            Text("Tareas Pendientes")
-
+    var isRefreshing by remember { mutableStateOf(false) }
+    val state = rememberPullToRefreshState()
+    val coroutineScope = rememberCoroutineScope()
+    val onRefresh: () -> Unit = {
+        isRefreshing = true
+        coroutineScope.launch {
+            tasksViewModel.obtenerTareas()
+            isRefreshing = false
         }
-        for (tarea in tareasPendientes) {
-            item {
-                CardTarea(
-                    tarea, tarea.usuario.username, {
-                        tasksViewModel.actualizarEstadoTarea(tarea._id)
-                        tasksViewModel.obtenerTareas()
-                    }, {
-                        tasksViewModel.eliminarTarea(tarea._id)
-                        tasksViewModel.obtenerTareas()
+    }
 
-                    }
+
+    PullToRefreshBox(
+        state = state,
+        isRefreshing = isRefreshing,
+        onRefresh = onRefresh,
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            item {
+
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
                 )
+                {
+                    HorizontalDivider(
+                        modifier = Modifier.weight(1f),
+                        thickness = 4.dp,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                    Text("Tareas Pendientes", Modifier.weight(2f).wrapContentWidth())
+                    HorizontalDivider(
+                        modifier = Modifier.weight(1f),
+                        thickness = 4.dp,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
 
             }
-        }
+            for (tarea in tareasPendientes) {
+                item {
+                    CardTarea(
+                        tarea, tarea.usuario.username, {
+                            tasksViewModel.actualizarEstadoTarea(tarea._id)
+                            tasksViewModel.obtenerTareas()
+                        }, {
+                            tasksViewModel.eliminarTarea(tarea._id)
+                            tasksViewModel.obtenerTareas()
 
-        item {
-            HorizontalDivider()
-        }
-        item {
+                        }
+                    )
 
-            Text("Tareas Completadas")
+                }
+            }
 
-        }
-        for (tarea in tareasCompletadas) {
             item {
-                CardTarea(
-                    tarea, tarea.usuario.username, {
-                        tasksViewModel.actualizarEstadoTarea(tarea._id)
-                        tasksViewModel.obtenerTareas()
+                HorizontalDivider()
+            }
+            item {
 
-                    }, {
-                        tasksViewModel.eliminarTarea(tarea._id)
-                        tasksViewModel.obtenerTareas()
-
-                    }
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
                 )
+                {
+                    HorizontalDivider(
+                        modifier = Modifier.weight(1f),
+                        thickness = 4.dp,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                    Text("Tareas Completadas", Modifier.weight(2f).wrapContentWidth())
+                    HorizontalDivider(
+                        modifier = Modifier.weight(1f),
+                        thickness = 4.dp,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
 
+            }
+            for (tarea in tareasCompletadas) {
+                item {
+                    CardTarea(
+                        tarea, tarea.usuario.username, {
+                            tasksViewModel.actualizarEstadoTarea(tarea._id)
+                            tasksViewModel.obtenerTareas()
+
+                        }, {
+                            tasksViewModel.eliminarTarea(tarea._id)
+                            tasksViewModel.obtenerTareas()
+
+                        }
+                    )
+
+                }
             }
         }
     }
+
 }
